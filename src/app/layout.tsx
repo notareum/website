424fc08b3f22c0e8a73cad216723f89d44e957ea
zaghadon <zaghadon@gmail.com>
2023-11-13 00:43:42 +0100

    Initial Project Setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+
+import { Inter } from 'next/font/google'
+import type { Metadata } from 'next'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Notareum - Streamlining Blockchain File Sharing and Collaboration',
+  description: 'Notareum Introduces a new UTI to streamline sharing blockchain based files using the .nota File Extension',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
